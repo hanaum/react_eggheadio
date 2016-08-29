c51c68fdd41aafaef9a578993007e3df4dcb701f
hanaum <hanau12@gmail.com>
2016-08-29 16:30:57 -0700

    Updating state of parent component.

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
       txt: 'this is the state text',
       cat: 0
     };
+    this.update = this.update.bind(this)
   }
 
   update(e) {
@@ -15,14 +16,26 @@ class App extends React.Component {
     })
   }
   render() {
+    // updating state.txt of the parent component
     return (
         <div>
-          <input type="text" 
-            onChange={this.update.bind(this)} />
-          <h1>{this.state.txt}</h1>
+          <Widget txt={this.state.txt} update={this.update} />
+          <Widget txt={this.state.txt} update={this.update} />
+          <Widget txt={this.state.txt} update={this.update} />
         </div>
       );
   }
+}
+
+// a stateless component
+const Widget = (props) => {
+    return (
+        <div>
+          <input type="text" 
+            onChange={props.update} />
+          <h1>{props.txt}</h1>
+        </div>
+      );
 }
 
 export default App
