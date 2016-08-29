ad3d87522b21d1d9bfe0dd2fb1261fabb300ec10
hanaum <hanau12@gmail.com>
2016-08-29 16:25:56 -0700

    Add state changes example.

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 class App extends React.Component {
+  constructor() {
+    // allow constructor to know what 'this' is.
+    super();
+    this.state = { 
+      txt: 'this is the state text',
+      cat: 0
+    };
+  }
+
+  update(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
   render() {
-    let txt = this.props.txt
     return (
         <div>
-          <h1>{txt}</h1>
-          <b>Bold</b>
+          <input type="text" 
+            onChange={this.update.bind(this)} />
+          <h1>{this.state.txt}</h1>
         </div>
       );
   }
 }
 
-// Keep track of the types that are expected.
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
-ReactDOM.render(
-  <App cat ={5} txt="this is the props value" />,
-  document.getElementById('app')
-);
+export default App
